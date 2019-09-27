baa7b0742b0652eeb2cb12cf0ec9aedfcdf6c051
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2019-09-27 18:34:48 +0900

    example
    혹시 import 잘 안되면 사용

---
+++ example_kotlin_calendar/app/src/main/java/com/example/example_kotlin_calendar/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.example_kotlin_calendar
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+   /* private fun monthlyCalendar(){
+        val calendarMin = Calendar.getInstance()
+        calendarMin.add
+    }*/
+}
