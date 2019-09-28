f5aaa4392ef74b1e698f6a88e01b3dab96fcc405
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2019-09-28 14:48:19 +0900

    Calendar sample
    현제 진행상황 sample

---
+++ example_kotlin_calendar2/app/src/main/java/com/example/example_kotlin_calendar2/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.example_kotlin_calendar2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.weekcalendar.*
+import kotlinx.android.synthetic.main.weekcalendar.view.*
+import com.applandeo.materialcalendarview.CalendarView
+import java.util.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constriant_main)
+
+
+
+    }
+
+}
