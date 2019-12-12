f483e3195213618710f9109b9f890a36d3592486
HwangSeonHyeok <55142789+HwangSeonHyeok@users.noreply.github.com>
2019-12-12 16:18:14 +0900

    delete all

--- mainlayout/daycalendar/src/main/java/com/example/daycalendar/hollyday.kt
+++
@@ -1,5 +0,0 @@
-package com.example.daycalendar
-
-class hollyday(val holymonth:Int = 1, val holydate:Int = 1, val holyInfo:String = "", val islunar:Boolean = false){
-
-}
--- mainlayout/weekcalendar/src/main/java/com/example/weekcalendar/hollyday.kt
+++
@@ -1,5 +0,0 @@
-package com.example.weekcalendar
-
-class hollyday(val holymonth:Int = 1, val holydate:Int = 1, val holyInfo:String = "", val islunar:Boolean = false){
-
-}
